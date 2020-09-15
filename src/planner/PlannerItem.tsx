574d5c38a810e1c58466f8c84cb2d6fcaa89eb8e
Matt Newbigging <mnewbigging@opus2.com>
2020-09-15 20:24:47 +0100

    styling planner day cards

--- src/planner/PlannerItem.tsx
+++ src/planner/PlannerItem.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-import { Card } from "@blueprintjs/core";
+import { Button, Card } from "@blueprintjs/core";
 
 import { IPlannerDay } from "./PlannerPageState";
-import { MealSelect } from "../components/MealSelect";
 
 import "./planner-item.scss";
 
@@ -11,10 +10,39 @@ export class PlannerItem extends React.Component<IPlannerDay> {
   public render() {
     return (
       <Card className={"day-item"}>
-        <div className={"day-item-date"}>{this.props.date}</div>
-        <div className={"day-item-breakfast"}>
-          <span>Breakfast:</span>
-          {/* <MealSelect key={this.props.id} /> */}
+        <div className={"day-item-row title"}>
+          <div className={"title-text"}>{this.props.date}</div>
+          <div>
+            <Button icon={"plus"} />
+          </div>
+        </div>
+        <div className={"day-item-row"}>
+          <div className={"day-item-label"}>Breakfast</div>
+          <div className={"day-item-meal"}>
+            <Button icon={"plus"} text={"Add meal"} />
+            <Button icon={"eye-open"} text={"Browse meals"} />
+          </div>
+        </div>
+        <div className={"day-item-row"}>
+          <div className={"day-item-label"}>Lunch</div>
+          <div className={"day-item-meal"}>
+            <Button icon={"plus"} text={"Add meal"} />
+            <Button icon={"eye-open"} text={"Browse meals"} />
+          </div>
+        </div>
+        <div className={"day-item-row"}>
+          <div className={"day-item-label"}>Dinner</div>
+          <div className={"day-item-meal"}>
+            <Button icon={"plus"} text={"Add meal"} />
+            <Button icon={"eye-open"} text={"Browse meals"} />
+          </div>
+        </div>
+        <div className={"day-item-row"}>
+          <div className={"day-item-label"}>Snacks</div>
+          <div className={"day-item-meal"}>
+            <Button icon={"plus"} text={"Add meal"} />
+            <Button icon={"eye-open"} text={"Browse meals"} />
+          </div>
         </div>
       </Card>
     );
